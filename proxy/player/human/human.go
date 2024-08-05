e3ce3fc2f04d4e49421668909bd4d8f4264f7e49
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2024-08-05 12:45:26 -0400

    Remove OPs from config

--- proxy/player/human/human.go
+++ proxy/player/human/human.go
@@ -52,11 +52,6 @@ type Human interface {
 
 	SendXUIDToAddon()
 
-	IsOP() bool
-
-	PlayerPermissions() byte
-	CommandPermissions() byte
-
 	SetOpenContainerWindowID(windowId byte)
 	SetOpenContainerType(containerType byte)
 	SetLastItemStackRequestID(id int32)
